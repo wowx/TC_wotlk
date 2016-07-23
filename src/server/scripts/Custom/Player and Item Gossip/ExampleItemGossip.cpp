6b20337a8d740b567ef2a7fcd2b20ebcebe316e0
Rochet2 <tqbattlenet@gmail.com>
2016-07-23 20:21:42 +0300

    Fix build after https://github.com/Rochet2/TrinityCore/commit/2056ce4d66cdaf12fe4e35c110508360026f9f7b

--- src/server/scripts/Custom/Player and Item Gossip/ExampleItemGossip.cpp
+++ src/server/scripts/Custom/Player and Item Gossip/ExampleItemGossip.cpp
@@ -11,7 +11,7 @@ class example_ItemGossip : public ItemScript
 public:
     example_ItemGossip() : ItemScript("example_ItemGossip") { }
 
-    bool OnUse(Player* player, Item* item, SpellCastTargets const& /*targets*/) override // Any hook here
+    bool OnUse(Player* player, Item* item, SpellCastTargets const& /*targets*/, ObjectGuid /*castId*/) override // Any hook here
     {
         player->PlayerTalkClass->ClearMenus(); // Clears old options
         player->ADD_GOSSIP_ITEM(0, "Morph", GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF + 1);
