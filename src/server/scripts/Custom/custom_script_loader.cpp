bced22774da1bf987a1fe579ab47a9b101ff041c
wowx <wowx@zoznam.sk>
2019-06-01 22:57:03 +0200

    Script/Beastmaster add

--- src/server/scripts/Custom/custom_script_loader.cpp
+++ src/server/scripts/Custom/custom_script_loader.cpp
@@ -17,11 +17,12 @@
 
 // This is where scripts' loading functions should be declared:
 void AddSC_Transmogrification();
-
+void AddSC_Npc_Beastmaster();
 
 // The name of this function should match:
 // void Add${NameOfDirectory}Scripts()
 void AddCustomScripts()
 {
     AddSC_Transmogrification();
+    AddSC_Npc_Beastmaster();
 }
